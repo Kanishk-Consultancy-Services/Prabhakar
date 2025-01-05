0977b04e5ad2504de9e44374a7f65148579e846c
vinaykumardarna2468 <your-email@example.com>
2025-01-05 15:13:36 +0530

    addition of scroll bar

--- KCS_WEB_Page/front_kcs_web/src/App.js
+++ KCS_WEB_Page/front_kcs_web/src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import MainPage from "./MainPage";
 import GoogleReviews from "./GoogleReviews";
 import Footer from "./Footer";
+import Scrolling from "./Scrolling";
 
 function App() {
   return (
     <>
     <MainPage></MainPage>
     <GoogleReviews></GoogleReviews>
+    <Scrolling></Scrolling>
     <Footer></Footer>
     </>
   );
---
+++ KCS_WEB_Page/front_kcs_web/src/Scrolling.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Scrolling.css'
+
+const Scrolling = () => {
+  return (
+  <>
+  <div className='scrollContainer'>
+   <marquee className='scrollingText'>
+    <p>Registration is going to get for 25% OFF on All our services upto XX-Feb CALL NOW +91 91083 18017 Hurry Up! 
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Registration is going to get for 25% OFF on All our services upto XX-Feb CALL NOW +91 91083 18017 Hurry Up! 
+  
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Registration is going to get for 25% OFF on All our services upto XX-Feb CALL NOW +91 91083 18017 Hurry Up!
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Registration is going to get for 25% OFF on All our services upto XX-Feb CALL NOW +91 91083 18017 Hurry Up!  
+    </p>
+    </marquee>
+  </div>
+  </>
+  )
+}
+
+export default Scrolling
