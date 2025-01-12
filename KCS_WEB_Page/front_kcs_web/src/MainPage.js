4b9ec82aaaaa250eda408eca334619cd48cd9226
venkat <Kanishkconsultancyservices2@gmail.com>
2025-01-12 17:52:02 +0530

    modified training.css file

--- KCS_WEB_Page/front_kcs_web/src/MainPage.js
+++ KCS_WEB_Page/front_kcs_web/src/MainPage.js
@@ -1,58 +1,65 @@
-import React from 'react'
-import './MainPage.css'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import { 
-    faFacebook,
-    faGoogle,
-    faPinterest,
-    faInstagram,
-    faLinkedin,
-    faTwitter,
-    faYoutube,
-} from '@fortawesome/free-brands-svg-icons';
-import LandingImage1 from './images/Let Kaapro find the right fit for your company_-Photoroom.png'
-import LandingImage2 from './images/Permanent Staffing Service Provider.jpg'
-import LandingImage3 from './images/MAINPAFE PIN.jpg'
-import KcsLogo1 from './images/KCS_Official_Logo.PNG'
-
+import React from "react";
+import "./MainPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGoogle,
+  faPinterest,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import LandingImage1 from "./images/Let Kaapro find the right fit for your company_-Photoroom.png";
+import LandingImage2 from "./images/Permanent Staffing Service Provider.jpg";
+import LandingImage3 from "./images/MAINPAFE PIN.jpg";
+import KcsLogo1 from "./images/KCS_Official_Logo.PNG";
 
 const MainPage = () => {
   return (
-
-      <div className='TopNavContainer'>
-      <div className='TopNavigationFieldsContainer'>
-        <div className='logo'>
-          <img className='KcsLogo' src={KcsLogo1} alt='KcsLogo'/>
-            {/* <h1>logo</h1> */}
+    <div className="TopNavContainer">
+      <div className="TopNavigationFieldsContainer">
+        <div className="logo">
+          <img className="KcsLogo" src={KcsLogo1} alt="KcsLogo" />
+          {/* <h1>logo</h1> */}
         </div>
-      <div className='TopNavigationFields'>
-      <p>Home</p>
-      <p>About Us</p>
-      <p>Trainings</p>
-      <p>Services</p>
-      <p>Others</p>
-      <p>Blog</p>
-      <p>Others</p>
-      <p>Contact Us</p>
+        <div className="TopNavigationFields">
+          <p>Home</p>
+          <p>About Us</p>
+          <p>Trainings</p>
+          <p>Services</p>
+          <p>Others</p>
+          <p>Blog</p>
+          <p>Others</p>
+          <p>Contact Us</p>
+        </div>
       </div>
+
+      <div className="imageContainer">
+        <center>
+          <img
+            className="LandingImage3"
+            src={LandingImage3}
+            alt="LandingImage"
+          />
+        </center>
       </div>
+    </div>
+  );
+};
 
-      <div className='imageContainer'>
-      <center><img className='LandingImage3'  src={LandingImage3} alt='LandingImage'/></center>
-      </div>
-      </div>
-  )
-}
+export default MainPage;
 
-export default MainPage
-
-
-
-{/* <div className='TopNavigation'>
+{
+  /* <div className='TopNavigation'>
           <div className='TopBarLine'>
-              <div className='TopBarText'> */}
-                  {/* <h1>&#128222; New Course Enquiry : +91 9346811821</h1> */}
-                  {/* <div className='AllIcons'>
+              <div className='TopBarText'> */
+}
+{
+  /* <h1>&#128222; New Course Enquiry : +91 9346811821</h1> */
+}
+{
+  /* <div className='AllIcons'>
                       <p><FontAwesomeIcon icon={faFacebook} /></p>
                       <p><FontAwesomeIcon icon={faGoogle} /></p>
                       <p><FontAwesomeIcon icon={faInstagram} /></p>
@@ -65,4 +72,5 @@ export default MainPage
 
               </div>
           </div>
-      </div> */}
+      </div> */
+}
