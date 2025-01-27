acd3aaa0cc82c1f0726b3c108bfa47665f3b0266
venkat <Kanishkconsultancyservices2@gmail.com>
2025-01-27 18:21:39 +0530

    made changes

--- KCS_WEB_Page/front_kcs_web/src/MainPage.js
+++ KCS_WEB_Page/front_kcs_web/src/MainPage.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./MainPage.css";
 import LandingImage1 from "./images/Permanent Staffing Service Provider.jpg";
 import LandingImage2 from "./images/securityagencies.webp";
 import LandingImage3 from "./images/MAINPAFE PIN.jpg";
+import "./EnquiryForm.css";
+
+import EnquiryForm from "./EnquiryForm";
 
 const MainPage = () => {
+  const carousel_images = [LandingImage1, LandingImage2, LandingImage3];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const moveSlide = (step) => {
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex + step;
+      if (newIndex >= carousel_images.length) return 0;
+      if (newIndex < 0) return carousel_images.length - 1;
+      return newIndex;
+    });
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      moveSlide(1); // Move to the next image every 3 seconds
+    }, 3000);
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, []);
   return (
     <div className="image-container">
       <div className="image-wrapper">
-        <img className="image" src={LandingImage1} alt="Landing1" />
-
         <img
           className="image"
-          width="100%"
-          src={LandingImage3}
-          alt="Landing2"
+          src={carousel_images[currentIndex]}
+          alt={`Images ${currentIndex + 1}`}
         />
 
-        <img className="image" src={LandingImage2} alt="Landing3" />
+        {/* <img className="image" src={LandingImage3} alt="Landing2" />
+
+        <img className="image" src={LandingImage2} alt="Landing3" /> */}
+      </div>
+      <div>
+        <EnquiryForm></EnquiryForm>
       </div>
     </div>
   );
